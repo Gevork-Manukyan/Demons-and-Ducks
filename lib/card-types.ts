840bef536e21e49027242da75c95a6d0eea1cb19
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-11-30 00:54:39 -0800

    creates card types

---
+++ lib/card-types.ts
@@ -0,0 +1,40 @@
+export type DeckType = "demon" | "duck";
+
+export type AbilityType =
+  | "draw1"
+  | "draw2"
+  | "draw3"
+  | "destroy"
+  | "summon"
+  | "repel"
+  | "displace"
+  | "swap"
+  | "hypnotize";
+
+export type CardEffect = AbilityType[];
+
+export type CardType = "creature" | "magic" | "instant";
+
+type BaseCard = {
+  name: string;
+  image: string;
+  effect: CardEffect;
+  deck: DeckType;
+  type: CardType;
+};
+
+export type CreatureCard = BaseCard & {
+  type: "creature";
+  isBasic: boolean;
+};
+
+export type MagicCard = BaseCard & {
+  type: "magic";
+};
+
+export type InstantCard = BaseCard & {
+  type: "instant";
+};
+
+export type Card = CreatureCard | MagicCard | InstantCard;
+
